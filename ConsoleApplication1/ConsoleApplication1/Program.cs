6fe84819ab19758458808a0d73220c8f828a01f8
talha945 <talha945@hotmail.com>
2017-11-07 14:47:37 +0500

    commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //welcome
+            Console.WriteLine("welcome to c# programming");
         }
     }
 }
